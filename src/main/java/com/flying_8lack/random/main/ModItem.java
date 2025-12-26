b404a703892e72e38f71d36d5467b608c9772946
flying_8lack <megacryleon@gmail.com>
2025-12-26 22:18:46 +0300

    Added fig food item.

--- src/main/java/com/flying_8lack/random/main/ModItem.java
+++ src/main/java/com/flying_8lack/random/main/ModItem.java
@@ -1,5 +1,10 @@
 package com.flying_8lack.random.main;
 
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.effect.MobEffects;
+import net.minecraft.world.food.FoodProperties;
+import net.minecraft.world.item.Item;
+import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
 import static com.flying_8lack.random.main.flying8lacksrandommod.MODID;
@@ -7,4 +12,17 @@ import static com.flying_8lack.random.main.flying8lacksrandommod.MODID;
 public class ModItem {
 
     public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(MODID);
+
+    public static final DeferredItem<Item> FIG_FOOD = ITEMS.register(
+            "fig_food", () -> new Item(new Item.Properties().food(
+                    new FoodProperties.Builder()
+                            .nutrition(4)
+                            .saturationModifier(3.6f)
+                            .effect(() -> new MobEffectInstance(
+                                    MobEffects.LUCK, 100
+                            ), 0.1f)
+                            .build()
+            )
+            )
+    );
 }
